f21053f383eb9d06c0a863f64eaef59408740c85
Anwar Chirakkattil <achirakkattil@pivotal.io>
2016-05-18 14:48:12 -0500

    Initial commit

---
+++ src/main/java/com/anwar/jenkins/plugin/JenkinsChangelogApplication.java
@@ -0,0 +1,12 @@
+package com.anwar.jenkins.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsChangelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsChangelogApplication.class, args);
+	}
+}
